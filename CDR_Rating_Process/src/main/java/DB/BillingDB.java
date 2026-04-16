23c45a319d4dc3be32e88289e9c873010b6cdcff
Ziad Osama <120169854+ziadosama74@users.noreply.github.com>
2026-04-16 09:35:02 +0200

    Refactor getConnection method formatting
    Refactor getConnection method for improved readability.

--- CDR_Rating_Process/src/main/java/DB/BillingDB.java
+++ CDR_Rating_Process/src/main/java/DB/BillingDB.java
@@ -10,17 +10,16 @@ public class BillingDB
     private static final String USER = "neondb_owner";
     private static final String PASSWORD = "npg_UqW3DZAinVE6";
     
-    public static Connection getConnection() {
+    public static Connection getConnection()
+    {
         Connection conn = null;
-
         try 
         {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
-         
         } 
         catch (SQLException e) 
         {
-            System.out.println("❌Connection failed!");
+            System.out.println("Connection failed");
             e.printStackTrace();
         }
         return conn;
