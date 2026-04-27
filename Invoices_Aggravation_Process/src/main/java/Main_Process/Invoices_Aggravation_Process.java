891841923c87acc585ad6ed0c7cf6b302d89a32c
Your Actual Name <fadymuhammad.fm@gmail.com>
2026-04-27 17:26:39 +0300

    update in invoices aggregation process

--- Invoices_Aggravation_Process/src/main/java/Main_Process/Invoices_Aggravation_Process.java
+++ Invoices_Aggravation_Process/src/main/java/Main_Process/Invoices_Aggravation_Process.java
@@ -1,13 +1,13 @@
 package Main_Process;
 
-import Billing.MonthlyBillingAggregationJob; // شيل الـ .java من هنا
+import Billing.MonthlyBillingAggregationJob;
 
 public class Invoices_Aggravation_Process {
 
     public static void main(String[] args) {
         System.out.println("🚀 Billing System Started...");
         
-        // استدعاء الميثود صح
+        
         MonthlyBillingAggregationJob.runMonthlyAggregation();
         
         System.out.println("🏁 Finished.");
